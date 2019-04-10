689e605191fbad786e2d5fbfd540a167487188c3
Heesang Chae <theand@gmail.com>
2019-04-10 20:18:12 +0900

    add utterance layout

--- md/assets/js/til.js
+++ md/assets/js/til.js
@@ -4,6 +4,7 @@ setTimeout(() => {
     document.querySelector(".utterances").style = `
       top: ${document.querySelector(".wy-nav-content").scrollHeight}px;
       min-height: 268px;
+      display: block;
     `;
   }
 }, 1000);

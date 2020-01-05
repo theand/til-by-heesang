c7ae446c37f236b5c9fe005915316eed00d1ab5e
Heesang Chae <theand@gmail.com>
2020-01-05 22:22:18 +0900

    til 2020-01-05 utterance style mod for mobile

--- md/assets/js/til.js
+++ md/assets/js/til.js
@@ -1,10 +1,58 @@
+const addGlobalStyle = (css) => {
+    const head = document.getElementsByTagName("head")[0];
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = css;
+    head.appendChild(style);
+};
+
+const utterancesStyle = `
+.utterances-frame{
+  position: relative;
+}
+
+html, .wy-body-for-nav{
+  overflow: visible;
+}
+
+.wy-nav-content-wrap {
+  background: #fcfcfc;
+}
+
+@media screen and (min-width: 1400px){
+  .utterances {
+    min-height: 268px;
+    display: block;
+    margin-left: 310px;
+  }
+}
+
+@media screen and (max-width: 768px){
+  .utterances {
+    min-height: 268px;
+    display: block;
+    margin-left: 2em;
+  }
+}
+`;
+
+addGlobalStyle(utterancesStyle);
 
 setTimeout(() => {
   if( document.querySelector(".utterances") ){
-    document.querySelector(".utterances").style = `
-      top: ${document.querySelector(".wy-nav-content").scrollHeight}px;
-      min-height: 268px;
-      display: block;
+    const dynamicStyle = `
+@media screen and (min-width: 1400px){
+  .utterances {
+    top: ${document.querySelector(".wy-nav-content").scrollHeight}px;
+  }
+}
+
+@media screen and (max-width: 768px){
+  .utterances {
+    top: ${document.querySelector(".wy-nav-content").scrollHeight + 30}px;
+  }
+}
     `;
+    addGlobalStyle(dynamicStyle);
   }
 }, 1000);
